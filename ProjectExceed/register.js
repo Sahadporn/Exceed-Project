4ce4896935ca66c741a5f870b8aab0f886932a1e
Chonchanat <76467581+chonchanat@users.noreply.github.com>
2021-02-20 01:24:35 +0700

    Update register.js

--- ProjectExceed/register.js
+++ ProjectExceed/register.js
@@ -3,17 +3,18 @@ const app = document.getElementById("dri-register")
 ///const cache = [];
 
 
-function scream(driver_name, cargo_id, time_start, source, destination) {
+function scream(driver_name, cargo_id, source, destination) {
   // Link here!!!
-  fetch("https://158.108.182.5:20004/driver/update", {
+  fetch("http://158.108.182.5:20004/driver/regis", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       driver_name: driver_name,
       cargo_id: cargo_id,
-      time_start: time_start,
       source: source,
-      destination: destination
+      // time_start: time_start,
+      destination: destination,
+      status: 1
     }),
   }).then((response) => console.log(response))
     .catch((error) => console.log("error", error));
@@ -22,14 +23,15 @@ function scream(driver_name, cargo_id, time_start, source, destination) {
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   driver_name = form.elements["driver_name"].value;
+  // console.log(form.elements["driver_name"].value);
   cargo_id = form.elements["cargo_id"].value;
-  time_start = form.elements["time_start"].value;
+  // time_start = form.elements["time_start"].value;
   source = form.elements["source"].value;
   destination = form.elements["destination"].value;
-  scream(driver_name, cargo_id, time_start, source, destination);
+  scream(driver_name, cargo_id, source, destination);
   form.elements["driver_name"].value = "";
   form.elements["cargo_id"].value = "";
-  form.elements["time_start"].value = "";
+  // form.elements["time_start"].value = "";
   form.elements["source"].value = "";
   form.elements["destination"].value = "";
 });
