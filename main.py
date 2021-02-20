9c512651ab3a6cba74c16c539511f6c90602db79
Narawish Sangsiriwut <narawish.s@ku.th>
2021-02-20 15:31:18 +0700

    Add comment
    add pydoc to function

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import datetime
-import math
-import time
 
 from flask import Flask, request
 from flask_pymongo import PyMongo
@@ -22,7 +20,7 @@ myCollection = mongo.db.test
 
 
 class CarDriver:
-    def __init__(self, name, car_id, source, dest, status):
+    def __init__(self, name, car_id, source, dest, status, min_temp, max_temp):
         self.driver_name = name
         self.car_id = car_id
         self.time_start = datetime.datetime.now()
@@ -33,6 +31,8 @@ class CarDriver:
         self.time = datetime.datetime.now()
         self.temp_his = [{"temp": 0, "time": self.time}]
         self.time_his = [{"time": self.time, "status": self.status}]
+        self.min_temp = min_temp
+        self.max_temp = max_temp
 
     def get_json(self):
         return {'driver_name': self.driver_name,
@@ -44,7 +44,9 @@ class CarDriver:
                 'temp': self.temp,
                 'time': self.time,
                 'temp_his': self.temp_his,
-                'time_his': self.time_his}
+                'time_his': self.time_his,
+                'min_temp': self.min_temp,
+                'max_temp': self.max_temp}
 
     def set_temp_his(self, temp_his):
         self.temp_his = temp_his
@@ -61,7 +63,13 @@ def init():
     query = myCollection.find()
     for d in query:
         if d['car_id'] not in car:
-            car[d['car_id']] = CarDriver(d['driver_name'], d['car_id'], d['source'], d['destination'], d['status'])
+            car[d['car_id']] = CarDriver(d['driver_name'],
+                                         d['car_id'],
+                                         d['source'],
+                                         d['destination'],
+                                         d['status'],
+                                         d['min_temp'],
+                                         d['max_temp'])
             car[d['car_id']].set_temp_his(d['temp_his'])
             car[d['car_id']].set_time_his(d['time_his'])
 
@@ -81,13 +89,15 @@ def driver_regis():
         'car_id': str,
         'source': str,
         'destination': str,
-        'status': 0/1/2
+        'status': 0/1/2,
+        'min_temp': str,
+        'max_temp': str
         }
     """
     data = request.json
     if data['car_id'] not in car:
         car[data['car_id']] = CarDriver(data['driver_name'], data['car_id'], data['source'], data['destination'],
-                                        data['status'])
+                                        int(data['status']), float(data['min_temp']), float(data['max_temp']))
         myCollection.insert_one(car[data['car_id']].get_json())
         return {"result": 'Driver Register Complete'}
     return {"result": 'Cargo already exist'}
@@ -96,8 +106,14 @@ def driver_regis():
 @app.route('/driver_update', methods=['POST'])
 @cross_origin()
 def driver_update():
-    print(request)
-    print(request.json)
+    """Update driver status
+
+    :arg
+    {
+        "car_id": str,
+        "status: 1/2
+    }
+    """
     data = request.json
     my_query = {'car_id': data['car_id']}
     query = myCollection.find(my_query)
@@ -119,6 +135,13 @@ def driver_update():
 @app.route('/temp_his', methods=['GET'])
 @cross_origin()
 def get_temp_his():
+    """GET temperature history
+
+    :return
+    {
+        "temp_his": [{"temp": float},{"time":time}]
+    }
+    """
     data = request.args.get('car_id')
     query = myCollection.find({'car_id': data})
     for d in query:
@@ -129,7 +152,12 @@ def get_temp_his():
 @app.route('/temp_input', methods=['POST'])
 @cross_origin()
 def temp_update():
-    """POST update temperature to database."""
+    """POST update temperature to database.
+
+    :args
+        "car_id": str,
+        "temp": int
+    """
     data = request.json
     my_query = {'car_id': data['car_id']}
     query = myCollection.find(my_query)
@@ -148,7 +176,23 @@ def temp_update():
 @app.route('/driver', methods=['GET'])
 @cross_origin()
 def find_driver_info():
-    """GET all driver information in database."""
+    """GET all driver information in database.
+
+    :return
+        {"result": ["driver_name": str,
+                    'car_id': str,
+                    'time_start': time,
+                    'source': str,
+                    'destination': str,
+                    "status": 0/1/2,
+                    "temp": float,
+                    "time": time,
+                    "temp_his": [{"temp": float}, {"time":time}],
+                    "time_his": [{"time": time}, {"status":0/1/2}],
+                    "min_temp": float,
+                    "max_temp": float
+                    ]}
+    """
     query = myCollection.find()
     driver_information = []
     for data in query:
@@ -163,64 +207,68 @@ def find_driver_info():
                 "temp": data["temp"],
                 "time": data["time"],
                 "temp_his": data["temp_his"],
-                "time_his": data["time_his"]
+                "time_his": data["time_his"],
+                "min_temp": data["min_temp"],
+                "max_temp": data["max_temp"]
             }
         )
     return {'result': driver_information}
 
 
-@app.route('/temp_input', methods=['GET'])
-@cross_origin()
-def find_temp_time():
-    """GET temp and total running time of car_id."""
-    data = request.args.get('car_id')
-    query = myCollection.find({'car_id': int(data)})
-    present_time = datetime.datetime.now()
-    for d in query:
-        if d['status'] == 0:
-            return {'result': {
-                'temp': d['temp'],
-                'time': 0
-            }}
-        if d['status'] == 1:
-            return {'result': {
-                'temp': d['temp'],
-                'time': (present_time - d['time_his'][-1]['time']).total_seconds()
-            }
-            }
-    return {'result': 'Error'}
-
-
 @app.route('/time', methods=['GET'])
 @cross_origin()
 def get_running_time():
+    """GET total running time after status 1.
+
+    :return
+        {"time": 0} if current status = 0
+        {"time": now - most recent status 1 time} if current status = 1
+        {"time": total deliverly time} if current status = 2
+    """
     data = request.args.get('car_id')
-    query = myCollection.find({'car_id': int(data)})
+    query = myCollection.find({'car_id': data})
     present_time = datetime.datetime.now()
     for d in query:
         if d['status'] == 0:
             return {'time': 0}
         if d['status'] == 1:
             return {'time': (present_time - d['time_his'][-1]['time']).total_seconds()}
+        if d['status'] == 2:
+            return {'time': (d['time_his'][-1]['time'] - d['time_his'][0]['time']).total_seconds()}
     return {'result': 'Error'}
 
 
 @app.route('/status', methods=['GET'])
 @cross_origin()
 def get_status():
+    """GET current status
+
+    :return
+        {"status": 0/1/2}
+    """
     data = request.args.get('car_id')
-    query = myCollection.find({'car_id': int(data)})
+    query = myCollection.find({'car_id': data})
     for d in query:
         return {'status': d['status']}
     return {'result': 'Error'}
 
 
-# @app.route('/reset', methods=['POST'])
-# @cross_origin()
-# def reset():
-#     """DELETE all data in database"""
-#     myCollection.delete_many({})
-#     return {'result': 'Delete Successfully'}
+@app.route('/alert', methods=['GET'])
+@cross_origin()
+def check_temp():
+    """Check if temperature is in threshold
+
+    :return
+        {alert: 0/1}
+    """
+    data = request.args.get('car_id')
+    query = myCollection.find({'car_id': data})
+    for d in query:
+        if d['temp'] <= d['min_temp'] or d['temp'] >= d['max_temp']:
+            return {'alert': 1}
+        else:
+            return {'alert': 0}
+    return {'result': 'Error'}
 
 
 if __name__ == "__main__":
