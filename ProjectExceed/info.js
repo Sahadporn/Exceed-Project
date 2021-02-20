c51f2000a73ecdef8e8bd9ad617602725aa8364e
Chonchanat <76467581+chonchanat@users.noreply.github.com>
2021-02-20 12:30:43 +0700

    Add files via upload

---
+++ ProjectExceed/info.js
@@ -0,0 +1,44 @@
+const url = 'http://158.108.182.5:20004/temp_his?car_id=1';
+const url2 = 'http://158.108.182.5:20004/driver_update';
+
+
+
+function send1() {
+    console.log("send");
+    fetch(url2,
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ car_id: "1", status: 1 })
+        }).then((response) => console.log(response)).then((a) => window.location.reload());
+    // console.log("Update status conplete!!!");
+}
+
+let button1 = document.getElementById("click1");
+
+button1.addEventListener("click", (event) => {
+    event.preventDefault();
+    send1();
+    //   window.location.reload();
+});
+
+
+
+function send2() {
+    console.log("send");
+    fetch(url2,
+        {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ car_id: "1", status: 2 })
+        }).then((response) => console.log(response)).then((a) => window.location.reload());
+    // console.log("Update status conplete!!!");
+}
+
+let button2 = document.getElementById("click1");
+
+button2.addEventListener("click", (event) => {
+    event.preventDefault();
+    send2();
+    //   window.location.reload();
+});
