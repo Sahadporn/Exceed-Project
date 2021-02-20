68404ae8e6a7913d9bfabe74291759f29c74af04
Chonchanat <76467581+chonchanat@users.noreply.github.com>
2021-02-20 17:11:54 +0700

    Update register.js

--- ProjectExceed/register.js
+++ ProjectExceed/register.js
@@ -3,7 +3,7 @@ const app = document.getElementById("dri-register")
 ///const cache = [];
 
 
-function scream(driver_name, car_id, source, destination) {
+function scream(driver_name, car_id, source, destination, min_temp, max_temp) {
   // Link here!!!
   fetch("http://158.108.182.5:20004/driver_regis", {
     method: "POST",
@@ -14,9 +14,13 @@ function scream(driver_name, car_id, source, destination) {
       source: source,
       // time_start: time_start,
       destination: destination,
-      status: 1
+      status: 1,
+      // waiting for database
+      min_temp: min_temp,
+      max_temp: max_temp
     }),
   }).then((response) => console.log(response))
+    .then(console.log("complete!"))
     .catch((error) => console.log("error", error));
 }
 
@@ -28,10 +32,15 @@ form.addEventListener("submit", (event) => {
   // time_start = form.elements["time_start"].value;
   source = form.elements["source"].value;
   destination = form.elements["destination"].value;
-  scream(driver_name, car_id, source, destination);
+  min_temp = form.elements["min_temp"].value;
+  max_temp = form.elements["max_temp"].value;
+
+  scream(driver_name, car_id, source, destination, min_temp, max_temp);
   form.elements["driver_name"].value = "";
   form.elements["car_id"].value = "";
   // form.elements["time_start"].value = "";
   form.elements["source"].value = "";
   form.elements["destination"].value = "";
+  form.elements["min_temp"].value = "";
+  form.elements["max_temp"].value = "";
 });
